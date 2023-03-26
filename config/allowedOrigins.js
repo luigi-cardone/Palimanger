2a773ede9a761a11d7e5319ca0ad29a4f38f115a
Luigi Cardone <lcardone22@gmail.com>
2023-03-27 01:00:35 +0200

    version 0.0.2 Publishing adaptations

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,9 @@
 const allowedOrigins = [
     'http://127.0.0.1:3000',
-    'palimanager-clo1.it',
-    'vps-6e47e4bb.vps.ovh.net',
+    'http://palimanager-clo1.it',
+    'https://palimanager-clo1.it',
+    'http://vps-6e47e4bb.vps.ovh.net',
+    'https://vps-6e47e4bb.vps.ovh.net',
     'http://localhost:3000',
     'http://localhost:8000'
 ]
